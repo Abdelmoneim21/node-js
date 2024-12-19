5cd2d63b6e8951b13659721dedef4693cbf2364f
Abdelmoneim21 <abdelmonemramadan9@gmail.com>
2024-12-19 03:54:19 +0200

    session-1

--- session-1/index.js
+++ session-1/index.js
@@ -7,7 +7,7 @@
 
 // console.log("file content", fileContent);
 
-/**local modules */
+/**Built IN modules */
 
 const os = require("node:os");
 const fs = require("node:fs");
@@ -15,3 +15,10 @@ console.log(os.homedir());
 console.log(os.platform());
 const fileContet = fs.readFileSync("./test.txt", "utf8");
 console.log("file content: ", fileContet);
+
+/*Local Modules*/
+
+const logger = require("./logger");
+
+logger.log("$ hello from logger $");
+logger.add(2, 6);
--- session-1/logger.js
+++ session-1/logger.js
@@ -1,4 +1,11 @@
 function logger(message) {
   console.log("this is message from logger", message);
 }
-module.exports = logger;
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+module.exports = {
+  log: logger,
+  add: add,
+};
