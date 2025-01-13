c972501450e539e09a15b322b6704458af426c7c
Abdelmoneim21 <abdelmonemramadan9@gmail.com>
2025-01-13 10:53:23 +0200

    session-5 cont

--- session-5/index.js
+++ session-5/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 
 const app = express();
 
+// use body parser for express to handle json post
+
+app.use(express.json());
 const courses = [
   {
     id: 1,
@@ -33,6 +36,23 @@ app.get("/api/courses/:id", (req, res) => {
     return res.status(404).json({ msg: "course not found" });
   }
   res.json(course);
+});
+
+/***************add new course (post) */
+
+app.post("/api/courses", (req, res) => {
+  console.log(req.body);
+
+  //test cases
+  if (!req.body.title) {
+    return res.status(400).json({ msg: "title not provided" });
+  }
+  if (!req.body.price) {
+    return res.status(400).json({ msg: "price not provided" });
+  }
+
+  courses.push({ id: courses.length + 1, ...req.body });
+  res.status(201).json(courses);
 });
 
 app.listen("5000", () => {
