6040cd71ff4773d15758b7875769f7d759f0b378
Abdelmoneim21 <abdelmonemramadan9@gmail.com>
2025-01-21 21:45:01 +0200

    splitting routes into modules

---
+++ session-5/controller/courses-controller.js
@@ -0,0 +1,62 @@
+let { courses } = require("../data/courses");
+const { body, validationResult } = require("express-validator");
+
+const getAllCourses = (req, res) => {
+  res.json(courses);
+};
+
+const getCourse = (req, res) => {
+  console.log(req.params);
+  const course = courses.find((course) => course.id === +req.params.id);
+  if (!course) {
+    return res.status(404).json({ msg: "course not found" });
+  }
+  res.json(course);
+};
+
+const addCourse = (req, res) => {
+  console.log(req.body);
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    returnres.status(400).json(errors.array());
+  }
+
+  //test cases
+  // if (!req.body.title) {
+  //   return res.status(400).json({ msg: "title not provided" });
+  // }
+  // if (!req.body.price) {
+  //   return res.status(400).json({ msg: "price not provided" });
+  // }
+  const course = { id: courses.length + 1, ...req.body };
+  courses.push(course);
+  res.status(201).json(course);
+};
+
+const updateCourse = (req, res) => {
+  const id = +req.params.id;
+  let course = courses.find((course) => course.id === id);
+  if (!course) {
+    return res.status(404).json({ msg: "course not found" });
+  }
+
+  course = { ...course, ...req.body };
+
+  res.status(200).json(course);
+};
+
+const deleteCourse = (req, res) => {
+  const id = +req.params.id;
+  //filterinf course to delete a targeted course
+  courses = courses.filter((course) => course.id != id);
+
+  res.status(200).json({ success: true });
+};
+
+module.exports = {
+  getAllCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+};
--- session-5/index.js
+++ session-5/index.js
@@ -5,38 +5,14 @@ const app = express();
 // use body parser for express to handle json post
 
 app.use(express.json());
-let courses = [
-  {
-    id: 1,
-    title: "js course",
-    price: 1000,
-  },
-  {
-    id: 2,
-    title: "react course",
-    price: 800,
-  },
-  {
-    id: 3,
-    title: "nodeJs course",
-    price: 800,
-  },
-];
 
+let { courses } = require("./data/courses");
+const coursesController = require("./controller/courses-controller");
 //get all courses
-app.get("/api/courses", (req, res) => {
-  res.json(courses);
-});
+app.get("/api/courses", coursesController.getAllCourses);
 
 //get single course
-app.get("/api/courses/:id", (req, res) => {
-  console.log(req.params);
-  const course = courses.find((course) => course.id === +req.params.id);
-  if (!course) {
-    return res.status(404).json({ msg: "course not found" });
-  }
-  res.json(course);
-});
+app.get("/api/courses/:id", coursesController.getCourse);
 
 /***************add new course (post) */
 
@@ -56,48 +32,15 @@ app.post(
       .isFloat({ min: 0 })
       .withMessage("price must be a non-negative number"),
   ],
-  (req, res) => {
-    console.log(req.body);
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      returnres.status(400).json(errors.array());
-    }
-
-    //test cases
-    // if (!req.body.title) {
-    //   return res.status(400).json({ msg: "title not provided" });
-    // }
-    // if (!req.body.price) {
-    //   return res.status(400).json({ msg: "price not provided" });
-    // }
-    const course = { id: courses.length + 1, ...req.body };
-    courses.push(course);
-    res.status(201).json(course);
-  }
+  coursesController.addCourse
 );
 
 //update course
 
-app.patch("/api/courses/:id", (req, res) => {
-  const id = +req.params.id;
-  let course = courses.find((course) => course.id === id);
-  if (!course) {
-    return res.status(404).json({ msg: "course not found" });
-  }
-
-  course = { ...course, ...req.body };
-
-  res.status(200).json(course);
-});
+app.patch("/api/courses/:id", coursesController.updateCourse);
 
 // delete course
-app.delete("/api/courses/:id", (req, res) => {
-  const id = +req.params.id;
-  //filterinf course to delete a targeted course
-  courses = courses.filter((course) => course.id != id);
-
-  res.status(200).json({ success: true });
-});
+app.delete("/api/courses/:id", coursesController.deleteCourse);
 
 app.listen("5000", () => {
   console.log("listening on port 5000");
